a38cd769ef5213ebe525b651bd40ccfb1c2d81af
sulfastor <torresmoisesa@gmail.com>
2025-02-10 21:49:58 -0600

    Push post

--- src/post/publish.rs
+++ src/post/publish.rs
@@ -1,4 +1,4 @@
-use git2::{Commit, ObjectType, Repository, Signature};
+use git2::{Commit, ObjectType, Repository, Signature, Direction};
 use std::path::Path;
 
 fn find_last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
@@ -9,8 +9,6 @@ fn find_last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
 
 pub fn add_and_commit_post(post_path: &Path, repo_path: &Path) -> Result<(), git2::Error> {
     let repo = Repository::open(repo_path)?;
-    let username = repo.config().unwrap().get_str("user.name").unwrap();
-    //  println!("{username}");
     let config = repo.config()?;
     let username = config
         .get_string("user.name")
@@ -19,22 +17,34 @@ pub fn add_and_commit_post(post_path: &Path, repo_path: &Path) -> Result<(), git
         .get_string("user.email")
         .map_err(|e| git2::Error::from_str(&format!("Failed to get user.email: {}", e)))?;
 
-    let mut index = repo.index()?;
-    index.add_path(post_path)?;
+    let rel_path = post_path
+        .strip_prefix(repo_path)
+        .expect("relative path is removed");
 
+    let mut index = repo.index()?;
+    index.add_path(rel_path)?;
     let oid = index.write_tree()?;
     let signature_bot = Signature::now("jekyll-poster", "jekyll-poster")?;
     let signature = Signature::now(&username, &email)?;
     let parent_commit = find_last_commit(&repo)?;
     let tree = repo.find_tree(oid)?;
 
+    let commit_msg = format!(
+        "Add new post entry: {}",
+        post_path.file_name().unwrap().to_str().unwrap()
+    );
     repo.commit(
         Some("HEAD"),
         &signature,
         &signature_bot,
-        &format!("Add new post entry: {}", post_path.display()),
+        &commit_msg,
         &tree,
         &[&parent_commit],
     )?;
+
+    let mut remote = repo.find_remote("origin")?;
+    remote.connect(Direction::Push)?;
+    remote.push(&["refs/heads/main:refs/heads/main"], None)?;
+
     Ok(())
 }
